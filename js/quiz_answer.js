9479fd03d875c98cc9a4c3b8d022077feddc5d19
GodOfTea <lomonosov66@mail.ru>
2019-05-02 00:32:08 +0500

    v0.1

---
+++ js/quiz_answer.js
@@ -0,0 +1,62 @@
+let answers = 
+{
+	"cd" : 
+	{
+		"description" : "Поздравляю, ты можешь стать отличным членом Competitions Department",
+		"image" : "cd.jpg",
+	},
+	
+	"crd" : 
+	{
+		"description" : "Поздравляю, ты можешь стать отличным членом Corparation Relations Department",
+		"image" : "crd.jpg",
+	},
+
+	"dd" : 
+	{
+		"description" : "Поздравляю, ты можешь стать отличным членом Design Department",
+		"image" : "dd.jpg",
+	},
+
+	"gd" : 
+	{
+		"description" : "Поздравляю, ты можешь стать отличным членом Grants Department",
+		"image" : "gd.jpg",
+	},
+
+	"vivaldi" : 
+	{
+		"description" : "Поздравляю, ты можешь стать отличным членом Vivaldi Department",
+		"image" : "vivaldi.jpg",
+	},
+
+	"td" : 
+	{
+		"description" : "Поздравляю, ты можешь стать отличным членом Training Department",
+		"image" : "td.jpg",
+	},
+
+	"prd" : 
+	{
+		"description" : "Поздравляю, ты можешь стать отличным членом Public Relations Department",
+		"image" : "prd.jpg",
+	},
+
+	"md" : 
+	{
+		"description" : "Поздравляю, ты можешь стать отличным членом Membership Department",
+		"image" : "md.jpg",
+	},
+
+	"eduid" : 
+	{
+		"description" : "Поздравляю, ты можешь стать отличным членом Educational Involvment Department",
+		"image" : "eduid.jpg",
+	},
+
+	"itd" : 
+	{
+		"description" : "Поздравляю, ты можешь стать отличным членом IT Department",
+		"image" : "itd.jpg",
+	},
+};
---
+++ js/quiz_code.js
@@ -0,0 +1,63 @@
+window.onload = function()
+{
+    let result = {};
+    let step = 0;
+
+    function showQuestion(questionNumber)
+    {
+        document.querySelector(".question").innerHTML = quiz[step]["q"];
+        let answer = "";
+        for (let key in quiz[step]["a"])
+        {
+            answer += `<li data-v="${key}" class="answer-varinat">${quiz[step]["a"][key]}</li>`;
+        }
+        document.querySelector(".answer").innerHTML = answer;
+    }
+
+    function showResult()
+    {
+        let key = Object.keys(result).reduce(function(a,b)
+        {
+            return result[a] > result[b] ? a : b;
+        });
+        console.log(key);
+
+        let div = document.createElement("div");
+        div.classList.add("result");
+        div.innerHTML = answers[key]["description"];
+        document.querySelector("main").appendChild(div);
+        let imgSrc = "img/" + key + ".jpg"
+        document.images["img"].src = imgSrc
+        console.log(imgSrc);
+    }
+
+    document.onclick = function(event)
+    {
+        event.stopPropagation();
+        if (event.target.classList.contains("answer-varinat") && step < quiz.length)
+        {
+            if (result[event.target.dataset.v] != undefined)
+            {
+                result[event.target.dataset.v]++;
+            }
+            else
+            {
+                result[event.target.dataset.v] = 1;
+            }
+            step++;
+            if (step == quiz.length)
+            {
+                document.querySelector(".question").remove();
+                document.querySelector(".answer").remove();
+                showResult();
+            }
+            else
+            {
+                showQuestion(step);
+            }
+            console.log(result);
+        }
+    }
+
+    showQuestion(step);
+}
